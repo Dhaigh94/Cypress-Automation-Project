b7f1dbb2c73efc696b1721a92f2b5c9e26182d67
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2019-07-12 17:48:36 +1000

    Add js file for the example.com automation

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,12 +1,12 @@
 describe('My first cypress test', () => {
-    it('Should load my Online Portfolio', function() {
-        cy.visit('/')
+    it('Should load Example Website', function() {
+        cy.visit('http://example.com/')
 
-        cy.url().should('include', 'https://www.davidhaighportfolio.com')
-        cy.title().should('eq', "David's Portfolio")
+        cy.url().should('include', 'http://example.com/')
+        cy.title().should('eq', "Example Domain")
 
         cy.get('h1').as('title')
-        cy.get('@title').contains('About Me')
+        cy.get('@title').contains('Example Domain')
         cy.get('@title').should('be.visible')
     })
 })
