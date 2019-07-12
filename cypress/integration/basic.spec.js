898316c5fb0933a24da227512572cc1403cb5e3a
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2019-07-12 18:26:27 +1000

    Change the example website

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,12 +1,12 @@
 describe('My first cypress test', () => {
-    it('Should load Example Website', function() {
-        cy.visit('http://example.com/')
+    it('Should load Fake Website', function() {
+        cy.visit('https://www.ultimateqa.com/fake-landing-page/')
 
-        cy.url().should('include', 'http://example.com/')
-        cy.title().should('eq', "Example Domain")
+        cy.url().should('include', 'https://www.ultimateqa.com/fake-landing-page/')
+        cy.title().should('eq', "Fake landing page - Ultimate QA")
 
         cy.get('h1').as('title')
-        cy.get('@title').contains('Example Domain')
+        cy.get('@title').contains('Learn to Code Websites, Apps & Games')
         cy.get('@title').should('be.visible')
     })
 })
