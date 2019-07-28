e4500c5024ef8272f4d99bf0b0d88ed9b9789109
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2019-07-28 13:28:57 +1000

    Add comments

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,15 +1,18 @@
 describe('My first cypress test', () => {
     it('Should load example.com', function() {
+        // Goes to example.com
         cy.visit('https://example.com/')
 
+        // The website shows in the url
         cy.url().should('include', 'https://example.com/')
+        // The website title shows in the tab
         cy.title().should('eq', 'Example Domain')
 
         cy.get('h1').as('title')
         cy.get('@title').contains('Example Domain')
         cy.get('@title').should('be.visible')
     })
-    it('Should load Google', function() {
+    it('Should load google.com.au', function() {
         cy.visit("https://google.com.au")
     })
 })
