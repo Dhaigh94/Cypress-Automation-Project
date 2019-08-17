c1babfe4f842592e826d50eb942479c244877c45
David Haigh <31473893+Dhaigh94@users.noreply.github.com>
2019-08-17 19:44:09 +1000

    Update Online Portfolio test

--- cypress/integration/Online Portfolio/online.portfolio.home.js
+++ cypress/integration/Online Portfolio/online.portfolio.home.js
@@ -1,15 +1,12 @@
-describe('My first Portfolio cypress test', () => {
-    it('Should load my Online Portfolio on the Home page', function() {
+describe('Portfolio cypress test on the home page', () => {
+    before(function() {
         cy.visit('/')
-
         cy.url().should('include', 'https://www.davidhaighportfolio.com')
         cy.title().should('eq', "David's Portfolio")
-
+    })
+    it('Check that the title on the home page says About Me', function() {
         cy.get('h1').as('title')
         cy.get('@title').contains('About Me')
         cy.get('@title').should('be.visible')
-    })
-    it('Should load not Online Portfolio Page', function() {
-        cy.visit("https://www.apple.com/au/")
     })
 })
